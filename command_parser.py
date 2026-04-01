b25419611c3c621546fa0118f7296c2b1c1f4167
ec-stacy <lit009ch@gmail.com>
2026-04-01 14:41:27 +0530

    command parser done

--- command_parser.py
+++ command_parser.py
@@ -1,60 +1,134 @@
-# action synonyms map (action -> token) 
-ACTION_SYNONYMS = {
-    "increase_brightness": ["increase", "raise", "boost", "brighten", "up", "higher", "more", "enhance", "intensify", "amplify", "maximize", "elevate", "heighten", "crank", "bump"],
-    "decrease_brightness": ["decrease", "lower", "reduce", "dim", "down", "less", "soften", "minimize", "darken", "drop", "shrink", "tone", "weaken", "fade", "cut"],
-    "set_brightness":      ["set", "put", "make", "change", "adjust", "fix", "assign", "configure", "define", "update", "apply", "move", "go", "shift", "push"],
+from command_ir import CommandIR    # class object to be returned
 
-    "increase_volume":     ["increase", "raise", "boost", "louder", "up", "higher", "more", "amplify", "maximize", "elevate", "crank", "bump", "turn", "enhance", "intensify"],
-    "decrease_volume":     ["decrease", "lower", "reduce", "quieter", "down", "less", "soften", "minimize", "drop", "shrink", "tone", "weaken", "fade", "cut", "mute"],
-    "set_volume":          ["set", "put", "make", "change", "adjust", "fix", "assign", "configure", "define", "update", "apply", "move", "go", "shift", "push"],
-    "mute_volume":         ["mute", "silence", "quiet", "hush", "kill", "suppress", "deafen", "stop", "disable", "shut", "drown", "block", "cut", "off", "zero"],
-
-    "open_application":    ["open", "launch", "start", "run", "execute", "load", "boot", "fire", "begin", "initiate", "activate", "bring", "pull", "access", "enter"],
-    "close_application":   ["close", "quit", "exit", "kill", "stop", "terminate", "shut", "end", "finish", "destroy", "remove", "collapse", "dismiss", "leave", "abandon"],
-
-    "increase_zoom":       ["zoom", "increase", "raise", "boost", "enlarge", "up", "higher", "more", "magnify", "maximize", "scale", "expand", "grow", "amplify", "in"],
-    "decrease_zoom":       ["unzoom", "decrease", "lower", "reduce", "shrink", "down", "less", "minimize", "scale", "contract", "compress", "out", "drop", "fade", "back"],
-
-    "lock_screen":         ["lock", "secure", "protect", "sleep", "block", "guard", "freeze", "seal", "hide", "suspend", "idle", "pause", "screen", "engage", "arm"],
-    "shutdown":            ["shutdown", "off", "poweroff", "halt", "stop", "kill", "terminate", "end", "close", "finish", "cut", "down", "power", "switch", "turn"],
-    "restart":             ["restart", "reboot", "reset", "reload", "refresh", "cycle", "renew", "reinitialize", "restore", "bounce", "redo", "start", "boot", "flip", "turn"],
+# verb resolution map
+VERB_SYNONYMS = {
+    "increase": ["increase", "raise", "boost", "brighten", "higher", "more", "enhance", "intensify", "amplify", "elevate", "heighten", "crank", "bump", "louder", "enlarge", "magnify", "expand", "grow"],
+    "decrease": ["decrease", "lower", "reduce", "dim", "less", "soften", "minimize", "darken", "drop", "shrink", "tone", "weaken", "fade", "cut", "quieter"],
+    "set":      ["set", "put", "make", "change", "adjust", "fix", "assign", "configure", "define", "update", "apply", "move", "go", "shift", "push"],
+    "mute":     ["mute", "silence", "quiet", "hush", "suppress", "deafen", "drown", "block", "zero"],
+    "open":     ["open", "launch", "start", "run", "execute", "load", "boot", "fire", "begin", "initiate", "activate", "bring", "pull", "access", "enter"],
+    "close":    ["close", "quit", "exit", "kill", "stop", "terminate", "shut", "end", "finish", "destroy", "remove", "collapse", "dismiss", "leave", "abandon"]
 }
 
-# generating flattened lookup dictionary from ACTION_SYNONYMS
-# token -> action
-flattened_dict = {} # verb resolution table
-for key, synonyms_list in ACTION_SYNONYMS.items():
+# generating flattened lookup dictionary from VERB_SYNONYMS (faster access)
+# token -> verb
+flattened_verb_dict = {} # verb resolution table
+for key, synonyms_list in VERB_SYNONYMS.items():
     for synonym in synonyms_list:
-        flattened_dict[synonym] = key
+        flattened_verb_dict[synonym] = key
 
-# target positional/lookup dictionary
-ACTION_TARGET_MODE = {
-    "increase_brightness": "lookup",
-    "decrease_brightness": "lookup",
-    "set_brightness":      "lookup",
-    "increase_volume":     "lookup",
-    "decrease_volume":     "lookup",
-    "set_volume":          "lookup",
-    "mute_volume":         "lookup",
-    "open_application":    "positional",
-    "close_application":   "positional",
-    "increase_zoom":       "lookup",
-    "decrease_zoom":       "lookup",
-    "lock_screen":         "none",
-    "shutdown":            "none",
-    "restart":             "none",
+# target resolution table
+TARGET_SYNONYMS = {
+    "brightness": ["brightness", "bright", "screen", "display", "backlight", "luminance", "glow"],
+    "volume":     ["volume", "sound", "audio", "noise", "speaker", "music"],
+    "zoom":       ["zoom", "scale", "magnification", "size"],
 }
 
-def command_parser(tokens: list):
-    for i, token in enumerate(tokens):
-        if token in flattened_dict:
-            action = flattened_dict[token]
+# flattened disk: synonym -> target
+flattened_target_dict = {}
+for key, synonyms_list in TARGET_SYNONYMS.items():
+    for synonym in synonyms_list:
+        flattened_target_dict[synonym] = key
+
+# no target required verbs
+STANDALONE_ACTIONS = {
+    "shutdown":  "shutdown",
+    "off":       "shutdown",
+    "poweroff":  "shutdown",
+    "halt":      "shutdown",
+    "power":     "shutdown",
+    "restart":   "restart",
+    "reboot":    "restart",
+    "reset":     "restart",
+    "reload":    "restart",
+    "refresh":   "restart",
+    "cycle":     "restart",
+    "renew":     "restart",
+    "reinitialize": "restart",
+    "restore":   "restart",
+    "bounce":    "restart",
+    "redo":      "restart",
+    "flip":      "restart",
+    "lock":      "lock_screen",
+    "secure":    "lock_screen",
+    "protect":   "lock_screen",
+    "sleep":     "lock_screen",
+    "guard":     "lock_screen",
+    "freeze":    "lock_screen",
+    "seal":      "lock_screen",
+    "hide":      "lock_screen",
+    "suspend":   "lock_screen",
+    "idle":      "lock_screen",
+    "pause":     "lock_screen",
+    "engage":    "lock_screen",
+    "arm":       "lock_screen",
+}
+
+# special action updation
+SPECIAL_ACTIONS = {
+    "open": "open_application",
+    "close": "close_application"
+}
+
+
+def command_parser(tokens: list) -> CommandIR:
+    command_ir = CommandIR()    # resulting intermediate representation
+
+    action_found = False  # flag
+    target_found = False
+    verb_found = False
+
+    verb, target = None, None
+
+    for i, token in enumerate(tokens): 
+        if token in STANDALONE_ACTIONS:
+            command_ir.action = STANDALONE_ACTIONS[token]
+            action_found = True
+            verb_found = True
+            target_found = True
             break
+        else:
+            if token in flattened_verb_dict:
+                verb = flattened_verb_dict[token]
+                verb_found = True
+
+                # open action command
+                if verb == 'open' or verb == 'close':
+                    if (i+1) < len(tokens):
+                        target = tokens[i+1]
+                        target_found = True
+                    else:
+                        command_ir.action = verb
+                        command_ir.errors.append("No target application provided!")
+                        return command_ir
+
+            if token in flattened_target_dict:
+                target = flattened_target_dict[token]
+                target_found = True
+            
+            if verb_found and target_found:
+                break
     
-    KNOWN_TARGETS = ["brightness", "volume", "zoom"]
-    if ACTION_TARGET_MODE[action] == "lookup":
-        for token in tokens:
-            if token in KNOWN_TARGETS:
-                target = token
-    elif ACTION_TARGET_MODE[action] == "positional":
-        target = tokens[i+1]
+    # error checking
+    if not verb_found:
+        command_ir.errors.append("No action found!")
+        return command_ir
+    
+    if not target_found:
+        command_ir.errors.append("No target found!")
+        return command_ir
+
+    # action & target creation (for non-standalone actions)
+    if not action_found:
+        if verb in SPECIAL_ACTIONS:
+            command_ir.action = SPECIAL_ACTIONS[verb]
+        else:
+            command_ir.action = verb + "_" + target
+        command_ir.target = target
+    
+    # finding parameters, if any
+    for token in tokens:
+        if token.isdigit(): # only for integers
+            command_ir.parameters["level"] = int(token)
+
+    return command_ir
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from input_processor import input_processor
+from command_parser import command_parser
 
 # taking in user input
 command = input("Enter your command: ")
 
 # input processing
 tokens = input_processor(command)
+print("Tokens generated: ", tokens)
 
-# checking output
-print("The output is as follows:\n", tokens)
+# parsing through the tokens and producing an intermediate representation
+ir = command_parser(tokens)
+print("The output is as follows:\n", ir)
